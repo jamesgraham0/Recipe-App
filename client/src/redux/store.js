32d166dfc90a5a7f0dfdd50b1480704b59dcfbb7
jamesgraham0 <jamesgraham0@users.noreply.github.com>
2022-08-04 17:20:44 -0700

    test

--- client/src/redux/store.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import recipesReducer from './recipes/reducer';
-
-export const store = configureStore({
-    reducer: {
-        recipes: recipesReducer
-    },
-    devTools: true
-});
